6d4fa69b1ee47a939a793d28ef48c887d519a2b5
JumpingKangaroo <lambertsonchris@gmail.com>
2019-09-27 16:53:34 -0400

    Start implementing importFromDB for Material

--- src/models/material.ts
+++ src/models/material.ts
@@ -1,5 +1,37 @@
 import sqlite3 from 'sqlite3';
+import { stringify } from 'querystring';
 
 class Material {
-    
+    // Declare class variables
+    name: string;
+    typeID: number;
+    volume: number;
+    groupID: number;
+    marketGroupID: number;
+    basePrice: number;
+    baseAmount: number;
+    complexMaterial: boolean;
+
+    importFromDB(db: sqlite3.Database) {
+        let sql: string = `SELECT * FROM invTypes WHERE
+            typeID=?`
+        db.get(sql, [this.typeID], (err, row) => {
+            if (err) {
+                return console.error(err.message);
+            }
+            this.groupID = row.groupID;
+            this.name = row.name;
+            this.volume = row.volume;
+            this.basePrice = row.basePrice;
+            this.marketGroupID = row.marketGroupID;
+            });
+        
+        
+    }
+
+    constructor(typeID: number, baseAmount: number, db: sqlite3.Database) {
+        this.typeID = typeID;
+        this.baseAmount = baseAmount;
+        this.importFromDB(db);
+    }
 }
